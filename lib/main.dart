0beb4c5dd592243d4ba94a97eade57e2c095b2c7
feizalfikri08 <feizalfikri8.com>
2019-09-24 11:01:09 +0700

    first time

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gojek/constans.dart';
+import 'package:gojek/launcher/launcher_view.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gojek',
+      theme: new ThemeData(
+        fontFamily: 'NeoSans',
+        primaryColor: GojekPalette.green,
+        accentColor: GojekPalette.green,
+      ),
+      home: new LauncherPage(),
+    );
+  }
+}
